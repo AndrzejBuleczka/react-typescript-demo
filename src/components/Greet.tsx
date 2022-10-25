a7d27f832681afb9f673fc2d084dabc9f1e66dfa
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-10-25 16:36:51 +0200

    more advanced props in TS

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,15 +1,17 @@
 type GreetProps = {
   name: string
-  messageCount: number
+  messageCount?: number
   isLogedIn: boolean
 }
 
 export const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props
   return (
     <div>
       <h2>
         {
-          props.isLogedIn ? `Welcome ${props.name}! You have ${props.messageCount} unread messages` 
+          props.isLogedIn 
+          ? `Welcome ${props.name}! You have ${messageCount} unread messages` 
           : 'Welcome Guest'
         }
       </h2>
